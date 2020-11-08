8dd31c7e13ca78cc3416b95e9688c2ad7da6ceb0
Mohsin <mohsin@xavor.com>
2020-11-09 00:20:30 +0500

    Case sensitive changes

---
+++ tests/phpunit/Inc/InpsydePluginTestCase.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1); # -*- coding: utf-8 -*-
+
+namespace InpsydePHPUnit\Inc;
+
+use PHPUnit\Framework\TestCase;
+use Brain\Monkey;
+
+/**
+ * An abstraction over WP_Mock to do things fast
+ */
+class InpsydePluginTestCase extends TestCase
+{
+
+    /**
+     * Setup which calls \WP_Mock setup
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        Monkey\setUp();
+        // A few common passthrough
+        // 1. WordPress i18n functions
+        Monkey\Functions\stubs(
+            [
+                '_n',
+                '_e',
+                'esc_attr',
+                'esc_html',
+                'esc_textarea',
+                '__',
+                '_x',
+                'esc_html__',
+                'esc_html_x',
+                'esc_attr_x',
+            ]
+        );
+    }
+
+    /**
+     * Teardown which calls \WP_Mock tearDown
+     *
+     * @return void
+     */
+    public function tearDown() : void
+    {
+        Monkey\tearDown();
+        parent::tearDown();
+    }
+}
